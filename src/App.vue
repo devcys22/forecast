0471af413839f5ea5b58873180dbbb37a9ccc2fe
dev22 <ysoli36512@gmail.com>
2024-08-21 06:27:28 +0900

    plus: resize 100

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup>
+import { onBeforeMount, onBeforeUnmount } from 'vue';
 import { RouterView } from 'vue-router'
+
+const init = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+onBeforeMount(() => {
+init();
+window.addEventListener("resize", init);
+})
+
+onBeforeUnmount(() => {
+window.removeEventListener("resize", init);
+})
 </script>
 
 <template>
