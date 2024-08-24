49525fe4f5a0cbde123be9e51ca07285f99c837f
dev22 <ysoli36512@gmail.com>
2024-08-24 23:45:14 +0900

    feat: homeview + api connection

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,58 @@
 <script setup>
-import { onBeforeMount } from 'vue';
-import axios from 'axios';
-onBeforeMount(async() => {
-  //서울 날씨 정보를 요청하는 API
-  const result = await axios.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/seoul?key=ZY7EJFHJCGKS83TZJLQGJDHG2&uniGroup=metric&lang=ko');
-  console.log(result);
-});
+import { useWeatherStore } from '@/stores/weather';
+import { storeToRefs } from 'pinia';
+import { computed, onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+    weatherStore.getCurrentWeatherInfo();
+})
+
+//서울 날씨 정보를 요청하는 API
+const weatherStore = useWeatherStore();
+const {currentConditions} = storeToRefs(weatherStore);
+const hourToMinutes = computed(() => {
+  const currentDate = new Date();
+  const currentHour = String(currentDate.getHours()).padStart(2, '0');
+  const currentMinute = String(currentDate.getMinutes()).padStart(2, '0');
+  return `${currentHour}:${currentMinute}`
+})
+const getImage = (path) => {
+  return new URL(`../assets/images/icons/${path}.png`, import.meta.url).href;
+}
+
 </script>
 
 <template>
-  <header class="header">
+  <header v-if="currentConditions" class="header">
       <!-- 지역 -->
       <h1 class="header__title">
         <span class="material-symbols-outlined"> location_on </span>서울
       </h1>
-      <h2 class="header__date">11:00</h2>
+      <h2 class="header__date">{{ hourToMinutes.value }}</h2>
     </header>
     <!-- 현재 날씨 -->
-    <main class="main">
+    <main v-if="currentConditions" class="main">
       <article class="weather">
         <section class="weather__info">
           <img
-            src="@/assets/images/icons/cloudy.png"
-            alt=""
+            :src="getImage(currentConditions.icon)"
+            :alt="`${currentConditions.datetime} ${currentConditions.temp}`"
             class="weather__img"
           />
-          <h3 class="weather_temp">28°</h3>
-          <p class="weather_summary">대체로 맑음</p>
+          <h3 class="weather_temp">{{currentConditions.temp}}</h3>
+          <p class="weather_summary">{{ currentConditions.conditions }}</p>
           <ul class="weather__moreList">
             <li class="weather__moreListItem">
               <p class="weather__subtitle">습도</p>
-              <p class="weather__desc">82%</p>
+              <p class="weather__desc">{{currentConditions.humidity}}</p>
             </li>
             <li class="weather__moreListItem">
               <p class="weather__subtitle">풍속</p>
-              <p class="weather__desc">4.12/ms</p>
+              <p class="weather__desc">{{ currentConditions.windspeed }}</p>
             </li>
             <li class="weather__moreListItem">
               <p class="weather__subtitle">체감</p>
-              <p class="weather__desc">7도</p>
+              <p class="weather__desc">{{currentConditions.feelslike}}</p>
             </li>
           </ul>
         </section>
